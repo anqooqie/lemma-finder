d04ed2c4a4b035e175af8352beb0be50057b0f3d
Hiroki FUKUDA <hfukuda@kde.cs.tsukuba.ac.jp>
2016-04-06 02:10:45 +0900

    不要なchompを削除

--- src/jp/ac/tsukuba/cs/kde/hfukuda/lemma_finder/Main.java
+++ src/jp/ac/tsukuba/cs/kde/hfukuda/lemma_finder/Main.java
@@ -7,21 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.commons.lang3.StringUtils;
-
 import net.didion.jwnl.JWNLException;
 import net.didion.jwnl.data.POS;
 
 public final class Main {
 	private Main() {}
 
-	public  static void main(final String[] args) throws IOException, JWNLException {
+	public static void main(final String[] args) throws IOException, JWNLException {
 		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				final List<String> lemmas = new ArrayList<>();
 				for (final POS pos : new POS[]{POS.NOUN, POS.VERB, POS.ADJECTIVE, POS.ADVERB}) {
-					lemmas.add(Optional.ofNullable(LemmaFinder.findLemma(StringUtils.chomp(line), pos)).orElse(""));
+					lemmas.add(Optional.ofNullable(LemmaFinder.findLemma(line, pos)).orElse(""));
 				}
 				System.out.println(String.join("\t", lemmas));
 			}
