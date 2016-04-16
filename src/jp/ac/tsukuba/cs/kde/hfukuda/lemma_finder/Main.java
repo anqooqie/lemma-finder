119cbfdb1b38e0bbcf79ff2bc6df74a9a911122c
Hiroki FUKUDA <hfukuda@kde.cs.tsukuba.ac.jp>
2016-04-16 15:47:43 +0900

    リソースリークの修正

--- src/jp/ac/tsukuba/cs/kde/hfukuda/lemma_finder/Main.java
+++ src/jp/ac/tsukuba/cs/kde/hfukuda/lemma_finder/Main.java
@@ -3,6 +3,7 @@ package jp.ac.tsukuba.cs.kde.hfukuda.lemma_finder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,9 +15,10 @@ public final class Main {
 	private Main() {}
 
 	public static void main(final String[] args) throws IOException, JWNLException {
-		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"))) {
+		try (final InputStreamReader isr = new InputStreamReader(System.in, StandardCharsets.UTF_8);
+				final BufferedReader br = new BufferedReader(isr)) {
 			String line;
-			while ((line = reader.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				final List<String> lemmas = new ArrayList<>();
 				for (final POS pos : new POS[]{POS.NOUN, POS.VERB, POS.ADJECTIVE, POS.ADVERB}) {
 					lemmas.add(Optional.ofNullable(LemmaFinder.findLemma(line, pos)).orElse(""));
